23cc5d09ae64ed383fd6fc1abac268ad51566ea6
ahmed-khaled93 <a7med.c2000@gmail.com>
2022-08-22 17:03:46 +0200

    fix recruitment form join us button css and change all files permissions to 644 and folders to 755

--- resources/views/frontend/e-services/recruitment.blade.php
+++ resources/views/frontend/e-services/recruitment.blade.php
@@ -15,12 +15,12 @@
             <div class="col-md-10 m-auto p-5 mt-5   component-modify">
                 @foreach ($jobs as $key => $job)
                   <div class="component-jop pb-4 mt-4">
-                    <div class="mt-2 d-flex justify-content-between">
-                      <div>
+                    <div class="mt-2 row justify-content-between">
+                      <div class="col-md-10">
                         <h6 class="title-jop">(@lang('Job'))</h6>
                         <h5 class="title-jop-defintion">{{ $job->job }} </h5>
                       </div>
-                      <div class="img-close">
+                      <div class="img-close col-md-2">
                         <a class="a-linke-join" href="{{ route('frontend.e-services.recruitment.apply', $job) }}">@lang('Join us')</a>
                       </div>
                     </div>
@@ -32,26 +32,6 @@
                     </div>
                   </div>
                 @endforeach
-
-                <!--job-tow-->
-                {{-- <div class="component-jop mt-4">
-                    <div class="mt-2 d-flex justify-content-between">
-                        <div>
-                            <h6 class="title-jop">(@lang('Job'))</h6>
-                            <h5 class="title-jop-defintion">مهندس معماري</h5>
-                        </div>
-                        <div class="img-close">
-                            <a class="a-linke-join" href="{{ route('frontend.e-services.recruitment.apply') }}">@lang('Join us')</a>
-                        </div>
-                    </div>
-                    <div class="col-md-10 mt-4">
-                        <h6  class="title-jop">(@lang('Information'))</h6>
-                        <pre class="pre-jobs mt-3">
-                            تحتاج شركه ايدرينك للمياه مهندس 3 سنين خبره علي الأقل مع المسؤولية
-                            الكامله للعمل الذي سيقوم بيه
-                         </pre>
-                    </div>
-                </div> --}}
             </div>
         </div>
     </section>
